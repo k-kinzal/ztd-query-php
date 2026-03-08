cb939719d96fef87825e1cbbbcbfb8d6f0b5cc6c
k-kinzal <keen.flag7803@logn.in>
2026-03-08 21:41:38 +0900

    Strengthen MySQL supported-language witnesses

--- packages/sql-faker/tests/Unit/MySql/SupportedLanguageTest.php
+++ packages/sql-faker/tests/Unit/MySql/SupportedLanguageTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\Unit\SqlFaker\MySql;
 
+use LogicException;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\UsesClass;
@@ -53,6 +54,37 @@ final class SupportedLanguageTest extends TestCase
         self::assertStringStartsWith('VALUES ROW(', $witness->sql);
     }
 
+    #[DataProvider('providerDeterministicWitnessSql')]
+    public function testGenerateWitnessUsesDeterministicCanonicalSqlForStableFamilies(
+        string $familyId,
+        string $expectedSql,
+    ): void {
+        $language = new SupportedLanguage('mysql-8.0.44');
+
+        $witness = $language->generateWitness(new FamilyRequest($familyId));
+
+        self::assertSame(1, $witness->seed);
+        self::assertSame($expectedSql, $witness->sql);
+    }
+
+    public function testGenerateWitnessRejectsUnknownParametersBeforeSearching(): void
+    {
+        $language = new SupportedLanguage('mysql-8.0.44');
+
+        $this->expectException(LogicException::class);
+        $this->expectExceptionMessage('Unknown parameters for family mysql.statement.select: unexpected');
+        $language->generateWitness(new FamilyRequest('mysql.statement.select', ['unexpected' => 1]));
+    }
+
+    public function testGenerateWitnessRejectsUnknownFamiliesBeforeDispatch(): void
+    {
+        $language = new SupportedLanguage('mysql-8.0.44');
+
+        $this->expectException(LogicException::class);
+        $this->expectExceptionMessage('Unknown family: mysql.unknown.family');
+        $language->generateWitness(new FamilyRequest('mysql.unknown.family'));
+    }
+
     /**
      * @param list<string> $expectedAnchorRules
      */
@@ -78,5 +110,19 @@ final class SupportedLanguageTest extends TestCase
     {
         yield 'mysql 8.0 source alias' => ['mysql-8.0.44', ['change']];
         yield 'mysql 8.4 dedicated statement' => ['mysql-8.4.7', ['change_replication_stmt']];
+    }
+
+    /**
+     * @return iterable<string, array{0: string, 1: string}>
+     */
+    public static function providerDeterministicWitnessSql(): iterable
+    {
+        yield 'statement any' => ['mysql.statement.any', 'BEGIN'];
+        yield 'statement select' => ['mysql.statement.select', 'WITH _i0 AS(SELECT _i1), _i2 AS(SELECT _i3) SELECT _i4 FOR UPDATE'];
+        yield 'statement insert' => ['mysql.statement.insert', 'INSERT HIGH_PRIORITY `_i0`._i1 SET _i2 = _i3'];
+        yield 'statement update' => ['mysql.statement.update', 'WITH _i0 AS(SELECT _i1), _i2 AS(SELECT _i3) UPDATE _i4 SET _i5 = _i6'];
+        yield 'statement delete' => ['mysql.statement.delete', 'WITH _i0 AS(SELECT _i1), _i2 AS(SELECT _i3) DELETE _i4 FROM _i5'];
+        yield 'create srs' => ['mysql.constraint.create_srs', 'CREATE SPATIAL REFERENCE SYSTEM IF NOT EXISTS 1 DESCRIPTION \'4fJTlWtA62\' ORGANIZATION \'ylRyZ1I\' IDENTIFIED BY 878115724 DEFINITION \'GEOGCS["WGS 84",DATUM["World Geodetic System 1984",SPHEROID["WGS 84",6378137,298.257223563]],PRIMEM["Greenwich",0],UNIT["degree",0.017453292519943278]]\' NAME \'9SrOoVZgqH1pllyYYGTpsyu65m6Hj\''];
+        yield 'alter database encryption' => ['mysql.constraint.alter_database.encryption', 'ALTER DATABASE _i0 ENCRYPTION \'Y\''];
     }
 }
