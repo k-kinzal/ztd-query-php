39a2e6e14ef165a1766dfe7e573736b83de43d25
k-kinzal <keen.flag7803@logn.in>
2026-08-17 22:50:47 +0900

    refactor(mysql): rely on token offsets for update sources

--- packages/ztd-query-mysql/src/UpdateSourceExtractor.php
+++ packages/ztd-query-mysql/src/UpdateSourceExtractor.php
@@ -24,7 +24,7 @@ final class UpdateSourceExtractor
                 continue;
             }
 
-            return trim(substr($source, $token->offset));
+            return substr($source, $token->offset);
         }
 
         return null;
