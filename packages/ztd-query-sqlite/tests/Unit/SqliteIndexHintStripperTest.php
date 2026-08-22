047342223159fdb8b1f4b8ab0982a577991ac805
k-kinzal <keen.flag7803@logn.in>
2026-08-22 21:10:02 +0900

    test: declare index hint parser dependency

--- packages/ztd-query-sqlite/tests/Unit/SqliteIndexHintStripperTest.php
+++ packages/ztd-query-sqlite/tests/Unit/SqliteIndexHintStripperTest.php
@@ -6,10 +6,12 @@ namespace Tests\Unit;
 
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\UsesClass;
 use PHPUnit\Framework\TestCase;
 use ZtdQuery\Platform\Sqlite\SqliteIndexHintStripper;
 
 #[CoversClass(SqliteIndexHintStripper::class)]
+#[UsesClass(\ZtdQuery\Platform\Sqlite\SqliteSelectRelationParser::class)]
 final class SqliteIndexHintStripperTest extends TestCase
 {
     public function testStripsIndexedByFromShadowSource(): void
