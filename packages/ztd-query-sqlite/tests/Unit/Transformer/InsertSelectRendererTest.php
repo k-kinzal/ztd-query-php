6f6573d74b0dee20f9c9aa35dc1bf19fe6d5c21d
k-kinzal <keen.flag7803@logn.in>
2026-08-22 19:02:30 +0900

    refactor(dialects): align insert-select renderer APIs

--- packages/ztd-query-sqlite/tests/Unit/Transformer/InsertSelectRendererTest.php
+++ packages/ztd-query-sqlite/tests/Unit/Transformer/InsertSelectRendererTest.php
@@ -32,4 +32,28 @@ final class InsertSelectRendererTest extends TestCase
             $sql,
         );
     }
+
+    public function testRendersGeneratedIdentityWithSqliteDialectSyntax(): void
+    {
+        self::assertSame(
+            '8 + ROW_NUMBER() OVER () - 1',
+            (new InsertSelectRenderer())->renderGeneratedIdentity(8),
+        );
+    }
+
+    public function testAcceptsMinimumGeneratedIdentityStart(): void
+    {
+        self::assertSame(
+            '1 + ROW_NUMBER() OVER () - 1',
+            (new InsertSelectRenderer())->renderGeneratedIdentity(1),
+        );
+    }
+
+    public function testRejectsNonPositiveGeneratedIdentityStart(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Generated identity start must be positive.');
+
+        (new InsertSelectRenderer())->renderGeneratedIdentity(0);
+    }
 }
