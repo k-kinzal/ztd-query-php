ff1858ea4d6cad5ed6e29d88e97318b3b7afac33
k-kinzal <keen.flag7803@logn.in>
2026-09-10 23:25:30 +0900

    test(sql-faker): complete value domain coverage declarations

--- packages/sql-faker/tests/Unit/SqliteProviderTest.php
+++ packages/sql-faker/tests/Unit/SqliteProviderTest.php
@@ -213,6 +213,9 @@ use UnexpectedValueException;
 #[UsesClass(\SqlFaker\Grammar\Generation\Value\QuotedDomain::class)]
 #[UsesClass(\SqlFaker\Grammar\Generation\Value\RepeatDomain::class)]
 #[UsesClass(\SqlFaker\Grammar\Generation\Value\WordDomain::class)]
+#[\PHPUnit\Framework\Attributes\UsesClass(\SqlFaker\Grammar\Generation\Value\OperatorDomain::class)]
+#[\PHPUnit\Framework\Attributes\UsesClass(\SqlFaker\Grammar\Generation\Value\RadixDomain::class)]
+#[\PHPUnit\Framework\Attributes\UsesClass(\SqlFaker\Grammar\Generation\Value\Utf8::class)]
 final class SqliteProviderTest extends TestCase
 {
     #[DataProvider('providerTargetedGenerationSeed')]
